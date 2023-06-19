fffc76bb598f1d4c90172b07cf39d56a5a8254f9
bryllim <bryllim@gmail.com>
2023-06-19 13:12:30 +0800

    todolist boilerplate

---
+++ todolist.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container p-5 m-5 border rounded shadow">
+        <h1 class="fw-bold">📋 Todo List</h1>
+        <form id="todoForm" class="my-3">
+            <input id="todoInput" class="form-control" type="text" placeholder="Add a todo item here...">
+            <button type="submit" class="btn btn-primary mt-2">Add</button>
+        </form>
+        <ul id="todoList" class="list-group">
+        </ul>
+    </main>
+
+    <script>
+        // Function to handle form submission
+        function handleSubmit(event) {
+            
+        }
+
+        // Function to fetch and display all todo items
+        function fetchTodoItems() {
+            
+        }
+    </script>
+</body>
+</html>
